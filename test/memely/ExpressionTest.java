e039224ce47213e5bc689af292a2d7254b940d09
lokman <lokman@mit.edu>
2021-03-10 02:13:55 -0500

    generated sample techsupport.png

--- test/memely/ExpressionTest.java
+++ test/memely/ExpressionTest.java
@@ -121,4 +121,10 @@ public class ExpressionTest {
         }
     }
     
+//    @Test
+//    public void sixPanelMeme() {
+//        String str = "img/black.png@600x50 ^ \"TECH SUPPORT\"@?x50 ---------------------------------- img/tech1.png | img/tech2.png@200x150 | img/tech3.png ---------------------------------- (  img/black.png@200x25 ^ \"What my friends think I do\"@?x15  | img/black.png@200x25 ^ \"What my mom thinks I do\"@?x15  | img/black.png@200x25 ^ \"What society thinks I do\"@?x15   ) ---------------------------------- img/black.png@600x25 ---------------------------------- img/tech4.png | img/tech5.png@200x160 | img/tech6.png ---------------------------------- (  img/black.png@200x25 ^ \"What my boss thinks I do\"@?x15   | img/black.png@200x25 ^ \"What I think I do\"@?x15  | img/black.png@200x25 ^ \"What I actually do\"@?x15       ) ---------------------------------- img/black.png@600x25";
+//        Expression.parse(str);
+//    }
+    
 }
